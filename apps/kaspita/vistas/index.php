b45a62e7f96d3e91c4bf57923adc284151eb0658
colab_01.diem <runtim3.error@gmail.com>
2013-02-01 11:32:21 -0700

    ...

--- apps/kaspita/vistas/index.php
+++ apps/kaspita/vistas/index.php
@@ -22,7 +22,7 @@
 	
 </head>
 <body >
-	<div class="pagina" style="border:#D6D6D6 1px solid;">
+	<div class="pagina" >
 		<script type="text/javascript">
 		/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
 		var disqus_shortname = 'kaspitastudios'; // required: replace example with your forum shortname
@@ -44,64 +44,67 @@
 	  fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));</script>
 		<div class="fb-like" data-send="false" data-width="450" data-show-faces="false" data-font="arial"></div>
-		<div class="header">
+		<div style="border:#b8b8b8 1px solid;margin-top:10px;">
+			<div class="header" style="margin-top:0 !important;">
+			
+					<div class='menubase'></div>
+					<?php $this->mostrar('/menu_principal'); ?>
+					
+					
+			</div>
 		
-				<div class='menubase'></div>
-				<?php $this->mostrar('/menu_principal'); ?>
-				
-				
-		</div>
+
+
+
 		
+		
+			<div class="sliderContainer">
+				<a href="#siguiente" class="next" title="Siguiente"></a>
+				<a href="#anterior" class="prev" title="Anterior"></a>
 
-
-
-
-		<div class="sliderContainer">
-			<a href="#siguiente" class="next" title="Siguiente"></a>
-			<a href="#anterior" class="prev" title="Anterior"></a>
-
-			<div class="slider slider_1">
-				<div class="slidesContainer">
-					<div class="slide"><img src="/web/apps/kaspita/imagenes/slide_grande_1.png" alt="Coche 01" /></div>
-					<div class="slide"><img src="/web/apps/kaspita/imagenes/slide_grande_1.png" alt="Coche 01" /></div>
-					<div class="slide"><img src="/web/apps/kaspita/imagenes/slide_grande_1.png" alt="Coche 01" /></div>
-					<div class="slide"><img src="/web/apps/kaspita/imagenes/slide_grande_1.png" alt="Coche 01" /></div>
-					<div class="slide"><img src="/web/apps/kaspita/imagenes/slide_grande_1.png" alt="Coche 01" /></div>
+				<div class="slider slider_1">
+					<div class="slidesContainer">
+						<div class="slide"><img src="/web/apps/kaspita/imagenes/slide_grande_1.jpg" alt="Coche 01" /></div>
+						<div class="slide"><img src="/web/apps/kaspita/imagenes/slide_grande_1.jpg" alt="Coche 01" /></div>
+						<div class="slide"><img src="/web/apps/kaspita/imagenes/slide_grande_1.jpg" alt="Coche 01" /></div>
+						<div class="slide"><img src="/web/apps/kaspita/imagenes/slide_grande_1.jpg" alt="Coche 01" /></div>
+						<div class="slide"><img src="/web/apps/kaspita/imagenes/slide_grande_1.jpg" alt="Coche 01" /></div>
+					</div>
+					<div class="fondo-paginador"></div>
+					<div class="selectorPagina"></div>
+					<div class="paginaActiva"></div>
+					<div class="paginador">
+						<div class="a-wrap" idw="idw1"><a cont="idw1" href="#">1</a></div>
+						<div class="a-wrap" idw="idw2"><a cont="idw2" href="#">2</a></div>
+						<div class="a-wrap" idw="idw3"><a cont="idw3" href="#">3</a></div>
+						<div class="a-wrap" idw="idw4"><a cont="idw4" href="#">4</a></div>
+						<div class="a-wrap" idw="idw5"><a cont="idw5" href="#">5</a></div>				
+					</div>
+					
 				</div>
-				<div class="fondo-paginador"></div>
-				<div class="selectorPagina"></div>
-				<div class="paginaActiva"></div>
-				<div class="paginador">
-					<div class="a-wrap" idw="idw1"><a cont="idw1" href="#">1</a></div>
-					<div class="a-wrap" idw="idw2"><a cont="idw2" href="#">2</a></div>
-					<div class="a-wrap" idw="idw3"><a cont="idw3" href="#">3</a></div>
-					<div class="a-wrap" idw="idw4"><a cont="idw4" href="#">4</a></div>
-					<div class="a-wrap" idw="idw5"><a cont="idw5" href="#">5</a></div>				
+			</div> 
+			<div class="explica" style="display:inline-block;margin-top:15px;margin-bottom:10px;">
+				<div style="display:inline-block;margin-right:40px;vertical-align:top">
+					<h3 style="display:inline;">&iquest;Quienes somos?</h3><br/>
+					<span style="margin-top: 8px;display:inline-block;">Un equipo de amantes al diseño con <br/>una manera especial de hacer las cosas...</span>
 				</div>
-				
+				<div style="display:inline-block;margin-right:40px;vertical-align:top">
+					<h3 style="display:inline;">Nuestro trabajo</h3><br/>
+					<span style="margin-top: 8px;display:inline-block;">Amamos lo que hacemos, y por ello<br /> tenemos excelentes resultados....</span>
+				</div>
+				<div style="display:inline-block;margin-right:40px;vertical-align:top">
+					<h3 style="display:inline;">Nuestra Ubicaci&oacute;n</h3><br/>
+					<span style="margin-top: 8px;display:inline-block;">En alguna parte de mazatlán, <br /> Sinaloa, Mexico...</span>
+				</div>			
+			</div>		
+			<div style="display:block;margin-bottom:10px;"></div>
+			<div style="background:#ffffff;padding-top:14px;padding-right:10px;">
+				<div class="telefonos" style="background-image:url('<?php echo $this->getRutaBase(); ?>imagenes/telefonos.png'); width:60%;height:600px;float:left;" ></div>
+				<div id="disqus_thread" style="width:30%;float:right;"></div>		
+				<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+				<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+				<div style="clear:both;"></div>
 			</div>
-		</div> 
-		<div class="explica" style="display:inline-block;margin-top:15px;margin-bottom:10px;">
-			<div style="display:inline-block;margin-right:40px;vertical-align:top">
-				<h3 style="display:inline;">&iquest;Quienes somos?</h3><br/>
-				<span>Un equipo de amantes al diseño con <br/>una manera especial de hacer las cosas...</span>
-			</div>
-			<div style="display:inline-block;margin-right:40px;vertical-align:top">
-				<h3 style="display:inline;">Nuestro trabajo</h3><br/>
-				<span>Amamos lo que hacemos, y por ello<br /> tenemos excelentes resultados....</span>
-			</div>
-			<div style="display:inline-block;margin-right:40px;vertical-align:top">
-				<h3 style="display:inline;">Nuestra Ubicaci&oacute;n</h3><br/>
-				<span>En alguna parte de mazatlán, <br /> Sinaloa, Mexico...</span>
-			</div>			
-		</div>		
-		<div style="display:block;margin-bottom:10px;"></div>
-		<div style="background:#ffffff;padding-top:14px;padding-right:10px;">
-			<div class="telefonos" style="background-image:url('<?php echo $this->getRutaBase(); ?>imagenes/telefonos.png'); width:60%;height:600px;float:left;" ></div>
-			<div id="disqus_thread" style="width:30%;float:right;"></div>		
-			<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-			<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
-			<div style="clear:both;"></div>
 		</div>
 	<div>
 </body>
