01c4c3129fbac0a1346f020768ecb02accfb38e3
colab_01.diem <runtim3.error@gmail.com>
2013-02-01 11:00:14 -0700

    ...

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	if ($a==0){
 		echo "El sistema está en mantenimiento, disculpe los inconvenientes.";		
 	}else{
-		header("Location: /paginas/index");
+		header("Location: /paginas/inicio");
 	}
 
 ?>
